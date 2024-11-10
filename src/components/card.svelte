c405cfd27be147d8df41f6bc156f251dcba2fe4c
Threeblade <sammmete@gmail.com>
2024-11-10 10:35:41 +1000

    Initial Commit

---
+++ src/components/card.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    var value = 2;
+    var suit = "heart"
+</script>
+
+<div class="card">
+
+    <div class="face">
+        <div class="badge-top">
+            <div class="badge-value">{{value}}</div>
+            <div class="badge-suit">{{suit}}</div>
+        </div>
+        <div class="badge-bottom">
+
+        </div>
+
+    </div>
+    <div class="back">
+
+    </div>
+</div>
+
+<style>
+    .card{
+        aspect-ratio: 3.5/2.5 auto;
+        min-width: 150px;
+    }
+    .face,.back{
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Card from "../components/card.svelte";
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Card></Card>
